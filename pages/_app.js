f4c9faf2446a2c12d2bdd4ce0b026a3a5ec9c7be
Biswash908 <167519939+Biswash908@users.noreply.github.com>
2024-05-17 17:31:52 +0545

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
-import React from "react";
+import { Fragment } from "react";
 import Head from "next/head";
 import "./global.css";
 
 function MyApp({ Component, pageProps }) {
-    return ( 
-      <React.Fragment>
-        <Head>
-        <title>threads-clone </title>
+  return (
+    <Fragment>
+      <Head>
+        <title>Threads App Clone (Community)</title>
         <meta
-          name = "viewport"
-          content = "minimum-scale=1, initial-scale=1, width=device-width"
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        </Head> 
-        <Component {...pageProps } />
-        </React.Fragment>
-    );
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp;
